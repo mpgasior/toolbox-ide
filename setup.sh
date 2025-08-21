f00261381c3c09e539c6360a6a471c5cf121cef1
nimelo <mr.nimelo@gmail.com>
2025-08-21 23:29:44 +0100

    yazi

--- setup.sh
+++ setup.sh
@@ -6,34 +6,36 @@ set -o pipefail
 set -o xtrace
 
 PACKAGES=(
-    fd
-    flatpak-xdg-utils
-    git
-    htop
-    lazygit
-    neovim
-    nnn
-    ripgrep
-    rustup
-    stow
-    tmux
+  fd
+  flatpak-xdg-utils
+  git
+  htop
+  lazygit
+  neovim
+  nnn
+  ripgrep
+  rustup
+  stow
+  tmux
+  yazi
 )
 
 COPR_REPOS=(
-    "atim/lazygit"
+  "atim/lazygit"
+  "lihaohong/yazi"
 )
 
 echo "Enabling COPR repositories: ${COPR_REPOS[@]}"
 
 for repo in "${COPR_REPOS[@]}"; do
-    echo "Attempting to enable COPR: $repo"
-    sudo dnf copr enable -y "$repo"
-    if [ $? -eq 0 ]; then
-        echo "Successfully enabled $repo"
-    else
-        echo "Failed to enable $repo. Please check the COPR name and your internet connection."
-    fi
-    echo "---"
+  echo "Attempting to enable COPR: $repo"
+  sudo dnf copr enable -y "$repo"
+  if [ $? -eq 0 ]; then
+    echo "Successfully enabled $repo"
+  else
+    echo "Failed to enable $repo. Please check the COPR name and your internet connection."
+  fi
+  echo "---"
 done
 
 echo "Updating the packages ..."
@@ -43,9 +45,9 @@ echo "Installing following packages: ${PACKAGES[@]}"
 dnf install "${PACKAGES[@]}" -y
 
 if [ $? -eq 0 ]; then
-    echo "Successfully installed packages."
+  echo "Successfully installed packages."
 else
-    echo "Failed to install packages"
+  echo "Failed to install packages"
 fi
 
 echo "Link flatpak-xdg-open with xdg-open ..."
