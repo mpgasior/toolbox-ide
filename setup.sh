0b43faa798d8a33ade166487c1fcd375e4a6c059
nimelo <mr.nimelo@gmail.com>
2025-10-29 20:46:31 +0000

    fedora 43 update (and fixes)

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o xtrace
-
 PACKAGES=(
   fd
   flatpak-xdg-utils
@@ -18,6 +13,7 @@ PACKAGES=(
   stow
   tmux
   yazi
+  zoxide
 )
 
 COPR_REPOS=(
@@ -34,6 +30,7 @@ for repo in "${COPR_REPOS[@]}"; do
     echo "Successfully enabled $repo"
   else
     echo "Failed to enable $repo. Please check the COPR name and your internet connection."
+    exit 1
   fi
   echo "---"
 done
@@ -48,12 +45,24 @@ if [ $? -eq 0 ]; then
   echo "Successfully installed packages."
 else
   echo "Failed to install packages"
+  exit 1
 fi
 
 echo "Link flatpak-xdg-open with xdg-open ..."
 ln -sf /usr/bin/flatpak-xdg-open /usr/bin/xdg-open
 
-echo "Installing TMUX Package Manager ..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+TPM_DIR="~/.tmux/plugins/tpm"
+if [[ -d "$TPM_DIR" ]]; then
+  echo "TMUX plugin manager already installed"
+else
+  echo "Installing TMUX Package Manager ..."
+  git clone https://github.com/tmux-plugins/tpm $TPM_DIR
+  if [ $? -eq 0 ]; then
+    echo "Successfully installed TMUX Package Manager."
+  else
+    echo "Failed to install packages"
+    exit 1
+  fi
+fi
 
 echo "Done."
